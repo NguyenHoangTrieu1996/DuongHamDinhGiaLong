87528aacda1992be7443567e668fe2833eeb8a64
NguyenHoangTrieu1996 <htbofficial1996@gmail.com>
2025-12-25 11:39:41 +0700

    123

--- page/chuyendekhac.js
+++ page/chuyendekhac.js
@@ -8,13 +8,13 @@ window.ChuyenDeKhac = function () {
         <div class="document-scroll">
             <ul class="lang lang-vi">
                 <li><a href="./public/vanhoa-main/index.html">📄 VĂN HÓA</a></li>
-                <li><a href="./public/kinhte/index.html">📄 KINH TẾ</a></li>
+                <li><a href="./public/kinhte-merge/index.html">📄 KINH TẾ</a></li>
                 <li><a href="./public/songnuoc-doc/page2.html">📄 SÔNG NGÒI KÊNH RẠCH</a></li>
             </ul>
 
              <ul class="lang lang-eng" style="dislay: none;">
                 <li><a href="./public/vanhoa-main/index.html">📄 CULTURE</a></li>
-                <li><a href="./public/kinhte/index.html">📄 ECONEMIC</a></li>
+                <li><a href="./public/kinhte-merge/index.html">📄 ECONEMIC</a></li>
                 <li><a href="./public/songnuoc-doc/page2.html">📄 RIVER</a></li>
             </ul>
         </div>
